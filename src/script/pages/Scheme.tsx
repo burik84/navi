5d0c63fcf4991f76b409dcdbd8d504054bf2a5ab
burik84 <avburik84@gmail.com>
2021-08-03 14:18:09 +0300

    feat: add logic for sheme

--- src/script/pages/Scheme.tsx
+++ src/script/pages/Scheme.tsx
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { useParams } from 'react-router-dom';
+import { AppContextSource } from '../services/AppContext';
+import { getData } from '../services/getData';
 
 import { Header } from '../layout/Header';
 import { Menu } from '../components/Menu';
+import { Lists } from '../components/Lists';
 
 import { getTitlePage } from '../shared/getTitlePage';
-import { IPageData } from '../shared/types';
+import { IPageData, IData } from '../shared/types';
 
-type key = {
-  section: string;
+const info: { [char: string]: string } = {
+  sug: 'sug',
+  us: 'us',
+  uhb: 'uhb',
+  ith: 'ith',
+  ups: 'ups',
+  ahz: 'ahz',
+  other: 'other',
+  ais: 'ais',
+  aov: 'aov',
+  rotork: 'rotork',
+  sgoes: 'sgoes',
 };
 
 export const Scheme: React.FC = () => {
+  const [listDataSection, setListDataSection] = useState<IData[] | []>([]);
+  const [listDataTitle, setListDataTitle] = useState<string[] | []>([]);
   const key: IPageData = useParams();
+
   const section: string = key.section || 'sug';
-  const title = getTitlePage(section, 'shemes');
+  const title = getTitlePage(section, 'scheme');
+  const { source = [] } = AppContextSource();
+
+  useEffect(() => {
+    const data = getData('scheme', source);
+    const dataSection = data.filter((item: IData) => item.folder === info[section]);
+
+    setListDataSection(dataSection);
+    const titleArray: string[] = [];
+    dataSection.forEach((item: IData) => {
+      if (!titleArray.includes(item.title)) {
+        titleArray.push(item.title);
+      }
+    });
+    setListDataTitle(titleArray);
+  }, [section]);
+
   return (
     <>
       <Header />
@@ -23,6 +55,7 @@ export const Scheme: React.FC = () => {
       <main>
         <h2>Схемы</h2>
         <p>{title}</p>
+        <Lists title={listDataTitle} data={listDataSection} />
       </main>
     </>
   );
